084b7ba70e805e6ab1f152f042878fe059968ebf
gilleswijnker <wijnker@pmtgroep.nl>
2017-06-16 21:21:07 +0200

    Adjusted main for debugging

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-import pbr.movement.movement as pbr_mv
+import math
 
-pbr_mv.drive(0.3)
-pbr_mv.spin(180.0)
-pbr_mv.drive(0.3, 0.8)
+
+TIME_FORWARD_1M = 5.7
+
+
+def _limit_rel_speed(rel_speed):
+    # Limit the relative speed:
+    #   0 < relative speed <= 1
+    rel_speed = max(min(rel_speed, 1.0), 0.0)
+    if rel_speed == 0:
+        raise ValueError
+    return rel_speed
+
+
+def corner_forward(angle, radius, rel_speed=1.0):
+    """
+    EXPERIMENTAL
+
+    Make a corner
+    """
+    rel_speed = _limit_rel_speed(rel_speed)
+    pi = math.pi
+    angle_rad = angle / 180.0 * pi
+
+    if angle < 0.0:
+        drive_left = 1.0 * rel_speed
+        drive_right = math.cos(angle_rad) * rel_speed
+    else:
+        drive_left = 1.0 * rel_speed
+        drive_right = -math.cos(angle_rad) * rel_speed
+
+    # Calculate the required time delay
+    meters = 2 * pi * radius * math.sin(angle/720.0 * pi)
+    num_seconds = meters * TIME_FORWARD_1M / rel_speed
+    print(num_seconds, drive_left, drive_right, meters)
+    # Perform the motion
+    # move(drive_left, drive_right, num_seconds)
+
+
+corner_forward(90, 1)
